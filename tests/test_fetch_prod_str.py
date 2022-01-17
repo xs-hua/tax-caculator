e164b721f4fe6526499e8ed2e244ea7ed5a2a65a
xinshu hua <work.hua.xinshu@gmail.com>
2022-01-17 21:23:09 +0100

    test: add test fetch prod is imported

--- tests/test_fetch_prod_str.py
+++ tests/test_fetch_prod_str.py
@@ -1,12 +1,21 @@
 import pytest
 
-from lib.fetch_input import fetch_product_sum
+from lib.fetch_input import fetch_product_sum, fetch_is_prod_imported
 
+#normal prod str
 @pytest.fixture
 def prod_inputs():
     prod_a = "1 book at 12.49"
     prod_b = "5 music CD at 14.99"
     return [prod_a, prod_b]
+
+#imported prod str
+@pytest.fixture
+def prod_imported_inputs():
+    prod_a = "1 book imported at 12.49"
+    prod_b = "5 imported music CD at 14.99"
+    prod_c = "2 music CD at 14.99"
+    return [prod_a, prod_b, prod_c]
 
 class TestTaxCaculator:
     #test fetch sum of correct str
@@ -17,5 +26,13 @@ class TestTaxCaculator:
         sum = fetch_product_sum(prod_inputs[1])
         assert sum == 5
     
-    
+    def test_fetch_prod_is_imported(self, prod_imported_inputs):
+        is_imported = fetch_is_prod_imported(prod_imported_inputs[0])
+        assert is_imported == True
+
+        is_imported = fetch_is_prod_imported(prod_imported_inputs[1])
+        assert is_imported == True
+
+        is_imported = fetch_is_prod_imported(prod_imported_inputs[2])
+        assert is_imported == False
 
