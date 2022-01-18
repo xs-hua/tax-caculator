1b72b368c2e484381cf379d2e66cf230ce18046e
xinshu hua <work.hua.xinshu@gmail.com>
2022-01-18 20:12:02 +0100

    feat: add round nr

--- lib/vali_input.py
+++ lib/vali_input.py
@@ -30,35 +30,19 @@ def caculate_tax(input_str):
     if is_prod_tax_free(fetch_prod_name(input_str)) == False:
         add_tax = add_tax + 0.1
 
-    return [round(price*add_tax, 3), round(price*add_tax-price, 3)]
+    return [round(price*add_tax, 2), round(price*add_tax-price, 2)]
 
+def round_nr(nr):
+    nr_str = str(int(nr*100))
+    last_nr = nr_str[-1]
 
-    '''
-
-    price_tax = round(price*add_tax, 2)
-    price_tax_str = str(int(price_tax*100))
+    #round number to 0.05
+    if int(last_nr) != 0 and int(last_nr) < 5:
+        nr_str = list(nr_str)
+        nr_str[-1] = '5'
+        nr = float("".join(nr_str)) / 100
     
-    #round price number to 0.05
-    if int(price_tax_str[-1]) < 5:
-        price_tax_str = list(price_tax_str)
-        price_tax_str[-1] = '5'
-        price_tax = float("".join(price_tax_str)) / 100
-        
-    tax = price*add_tax - price
-    tax_str = str(int(tax*100))
-
-    print(tax)
-
-    #round tax number to 0.05
-    if tax != 0.0 and int(tax_str[-1]) != 0 and int(tax_str[-1]) < 5:
-        tax_str = list(tax_str)
-        tax_str[-1] = '5'
-        tax = float("".join(tax_str)) / 100
-
-    return [price_tax, tax]
-
-    '''
-
+    return nr
 
     
     
