efee6987a9c6496f8143276aebb3725eea1c67e8
xinshu hua <work.hua.xinshu@gmail.com>
2022-01-18 17:25:04 +0100

    test: add test tax caculation

--- tests/test_vali_input.py
+++ tests/test_vali_input.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lib.vali_input import is_prod_tax_free
+from lib.vali_input import is_prod_tax_free, caculate_tax
 
 #normal prod name
 @pytest.fixture
@@ -16,7 +16,15 @@ def prod_names():
 
     return prod_list
 
+#prod with tax
+@pytest.fixture
+def prod_taxes():
+    prod_list = []
+    prod_list.append(["1 book at 12.49", 12.49, 0.0])
+    prod_list.append(["1 imported box of chocolates at 10.00", 10.5, 0.5])
+    prod_list.append(["1 imported bottle of perfume at 47.50", 54.625, 7.125 ])
 
+    return prod_list
 
 
 class TestInputValidation:
@@ -26,6 +34,8 @@ class TestInputValidation:
         for name in prod_names:
             assert is_prod_tax_free(name[0]) == name[1]
      
-            
-
+    def test_tax_caculation(self, prod_taxes):
+        for p in prod_taxes:
+            assert caculate_tax(p[0])[0] == p[1]
+            assert caculate_tax(p[0])[1] == p[2]
         
