bd772718f3311cd40288dc0c9c6f541717575f06
xinshu hua <work.hua.xinshu@gmail.com>
2022-01-18 21:16:31 +0100

    test: add test tax cacul and round nr toget

--- tests/test_vali_input.py
+++ tests/test_vali_input.py
@@ -1,6 +1,7 @@
 import pytest
 
 from lib.vali_input import is_prod_tax_free, caculate_tax, round_nr
+from lib.fetch_input import fetch_prod_price
 
 #normal prod name
 @pytest.fixture
@@ -25,11 +26,35 @@ def prod_taxes():
 
     return prod_list
 
-#
+#data for testing nr rounding to 0.05
 @pytest.fixture
 def unrounded_nrs():
     return [[6.10, 6.10], [6.12, 6.15], [6.18, 6.18]]
 
+#prod with tax
+@pytest.fixture
+def prods():
+    prod_list = []
+    prod_list.append(
+        [
+        ["1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85"], 
+        29.83,
+        1.5
+        ])
+    prod_list.append(
+        [
+        ["1 imported box of chocolates at 10.00", "1 imported bottle of perfume at 47.50"], 
+        65.15,
+        7.65
+        ])
+    prod_list.append(
+        [
+        ["1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"], 
+        74.68,
+        6.7
+        ])
+    
+    return prod_list
 
 class TestInputValidation:
 
@@ -48,5 +73,17 @@ class TestInputValidation:
         for nr in unrounded_nrs:
             assert round_nr(nr[0]) == nr[1]
         
+    def test_tax_cacul_and_round_nr(self, prods):
 
+        for p in prods:
+            origin_price = 0.0
+            price_sum = 0.0
+            
+            for item in p[0]:
+                origin_price += fetch_prod_price(item)
+                result = caculate_tax(item)
+                price_sum += round_nr(result[0])
+
+            assert round(price_sum, 2) == p[1]
+            assert round(price_sum - origin_price, 2) == p[2]
             
